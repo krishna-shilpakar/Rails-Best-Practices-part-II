2ca3d9c2b9050ece74c418d0a885835918db9df1
Krishna Shilpakar <krishna@sprout-technology.com>
2011-05-30 09:55:12 +0545

    to_s method in comment. Not fully completed.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   validates_length_of :text, :minimum => 4
   scope :recent, lambda {|count ||= nil|  order("created_at DESC").limit(count)}
-  def commenter_address
-    "#{self.user.city},#{self.user.country}"
+  def to_s
+    "#{self.user.city}, #{self.user.country}"
   end
 end
 

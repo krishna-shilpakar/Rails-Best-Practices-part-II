d2f69ea65578774e2b73299e802eb63e5948ed19
Krishna Shilpakar <krishna@sprout-technology.com>
2011-05-30 09:26:51 +0545

    seo friendly url for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ActiveRecord::Base
  has_many :comments
 belongs_to :user
 
+
+def to_params
+  "#{self.id} - #{self.title.parameterize}"
+end
+
 def recent_commenters
     self.comments.recent(5).includes(:user).collect{|comment| comment.user.login}.map{|commenter| "<li>#{commenter}</li>"}.join("")
  end

11df12993fec0aac088da65db1047052a9822f84
Krishna Shilpakar <krishna@sprout-technology.com>
2011-05-30 09:52:42 +0545

    aligned text in post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,15 @@
 class Post < ActiveRecord::Base
- has_many :comments
-belongs_to :user
   has_many :comments, :counter_cache => true # it requires to add comments_count in post model
   belongs_to :user
   validates_presence_of :title, :text
 
+  def to_params
+    "#{self.id} - #{self.title.parameterize}"
+  end
 
-def to_params
-  "#{self.id} - #{self.title.parameterize}"
-end
-
-def recent_commenters
+  def recent_commenters
     self.comments.recent(5).includes(:user).collect{|comment| comment.user.login}.map{|commenter| "<li>#{commenter}</li>"}.join("")
- end
+  end
 
 end
 

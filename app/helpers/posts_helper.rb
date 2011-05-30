631e3909c7612ff0c12c95ecbfb5e6aaf031d7f6
Krishna Shilpakar <krishna@sprout-technology.com>
2011-05-30 09:08:59 +0545

    Added helper method called list_posts(args).

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def list_posts(posts)
+    raw( posts.each do |post|
+      content_tag(:h4) do
+        link_to "#{post.title} (#{post.comments.length})", user_post_path(current_user,post)
+      end + post.text
+    end.join )
+  end
 end

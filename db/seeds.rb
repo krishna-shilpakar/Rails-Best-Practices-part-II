5ce9d7a02786cdef7c3263436cda6701191f90f4
Krishna Shilpakar <krishna@sprout-technology.com>
2011-05-30 09:52:22 +0545

    Move to seed data. but not completely effective.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+
+# sample users with addresses
+
+
+#sample post
+
+#create sample users
+User.find_or_create_by(:login => "Matthew", :password => "matt", :password_confirmation => "matt")
+User.find_or_create_by(:login => "Gregory", :password => "greg", :password_confirmation => "greg")
+ User.first.posts.create(:title => "Hello World", :text => "My first blog post.")
+ 
+ 
+user = User.first
+user.create_address(:country=>"USA",:city => "NY")
+user = User.last
+user.create_address(:country=>"Canada",:city => "AB")
